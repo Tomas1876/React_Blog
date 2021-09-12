2b90b18a34c5711156b9d3321f11585be967d8e1
Tomas1876 <mnd3259@gmail.com>
2021-09-12 17:18:09 +0900

    회원가입 기능 구현

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../module/auth';
+import { changeField, initializeForm, register } from '../../module/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm = () => {
     //생성한 action을 useDispatch를 통해 발생시킬 수 있다
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.register
+    const { form, auth, authError } = useSelector(({ auth }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
     }));
 
     //인풋 변경 핸들러
@@ -16,15 +18,35 @@ const RegisterForm = () => {
         dispatch(changeField({ form: 'register', key: name, value }));
     }
     //폼 등록 이벤트 핸들러
+    //이 이벤트 발생시 reigster 함수에 username과 password를 파라미터로 넣어 디스패치 해준다
     const onSubmit = e => {
         e.preventDefault();
-
+        const { username, password, passwordConfirm } = form;
+        if (password != passwordConfirm) {
+            // 오류처리 해야 함
+            return;
+        }
+        dispatch(register({ username, password }));
     }
 
     //컴포넌트가 처음 렌더링 될 때 form을 초기화함
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    //회원가입 성공/실패 처리
+    //사가에서 API 요청을 처리하고, 이에 대한 결과는 auth/authError를 통해 조회할 수 있다
+    //auth, authError 중 어떤 값이 유효한지에 따라 다른 작업 수행
+    useEffect(() => {
+        if (authError) {
+            console.log('오류발생')
+            console.log(authError)
+        }
+        if (auth) {
+            console.log('회원가입 성공')
+            console.log(auth);
+        }
+    }, [auth, authError])
 
     return (
         <AuthForm type='register'
